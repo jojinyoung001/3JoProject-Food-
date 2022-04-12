92a2f6af9a756d11cd449205505dd4c094cb77c3
kichan <ckcc21@gmail.com>
2022-04-12 13:09:23 +0900

    식당 조회 기능 추가

--- Project/Main.java
+++ Project/Main.java
@@ -1,36 +1,46 @@
 package Project;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Food{
+class Food {
 	private String food;
-	public String getFood() {return food;}
-	public void setFood(String food) {this.food = food;}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
 }
 
 public class Main {
-public static void main(String[] args) {
-	ArrayList<Food> fd = new ArrayList<Food>();
-	
-	Scanner sc = new Scanner(System.in);
-	while(true) {
-		System.out.println("1.식당 입력");
-		System.out.println("2.식당 조회");
-		System.out.println("3.식당 수정");
-		System.out.println("4.식당 뽑기");
-		int ch;
-		ch = sc.nextInt();
-		switch(ch) {
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
+	public static void main(String[] args) {
+		ArrayList<Food> fd = new ArrayList<Food>();
+
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("1.식당 입력");
+			System.out.println("2.식당 조회");
+			System.out.println("3.식당 수정");
+			System.out.println("4.식당 뽑기");
+			int ch;
+			ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				break;
+			case 2:
+				for (int i = 0; i < fd.size(); i++) {
+					System.out.println((i + 1) + "번째 식당 : " + fd.get(i).getFood());
+				}
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			}
 		}
+
 	}
-	
-}
 }
